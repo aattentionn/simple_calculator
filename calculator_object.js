186a8545b0ef744162b928df4caf5e0b786350fd
aattentionn <nikeshina.anna1998@gmail.com>
2022-01-31 23:12:49 +0300

    added object to calculator

--- calculator_object.js
+++ calculator_object.js
@@ -1,147 +1,23 @@
-function Calc(operator, num1, num2) {
+function Calc(operator, num1, num2){
 
-    let isNotValid = (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof operator !== 'undefined')
-    if (isNotValid){
-      return 'Error';
-    }
-
-    let operations = {
-        sum: 'num1 + num2',
-        sub: 'num1 - num2',
-        multi: 'num * num2',
-        div: 'num1 / num2',
-      };
-
-      alert(operations[operator]);
-    
-
-
+  if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+    return 'Error';
 }
-
-console.log(Calc('sum', 9, 3));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 let operations = {
-    sum: 'num1+num2',
-    sub: 'num1-num2',
-    multi: 'num*num2',
-    div: 'num1/num2',
+  sum: num1+num2,
+  sub: num1-num2,
+  multi: num1*num2,
+  div: num1/num2,
+  '': 'Unknown operation',
+}
+
+return operations[operator];
 
 }
-num1=2;
-num2=3;
-
-let result = prompt('Which operation should I do?', 'sum', 'sub', 'multi', 'div');
-
-alert(operations[result]);
-
-
-function Calc(num1, num2) {
-    return {
-      num1 = 2;
-      num2 = 4;
-    };
-
-    let operations = {
-        sum: 'num1+num2',
-        sub: 'num1-num2',
-        multi: 'num*num2',
-        div: 'num1/num2',
-    
-    }
-}
-    let result = prompt('Which operation should I do?', 'sum', 'sub', 'multi', 'div');
-
-alert(operations[result]);
-
-let result = prompt('Which operation should I do?', 'sum', 'sub', 'multi', 'div')
-
-  let operations = {
-
-    sum: 'num1 + num2',
-    sub: 'num1 - num2',
-    multi: 'num * num2',
-    div: 'num1 / num2',
-  
-  }
-
-
-alert(operations[sum]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let result;
-  const isNotValid = (typeof num1 !== 'number' || typeof num2 !== 'number')
-    if (isNotValid){
-      result = 'Error';
-    }
-    else if (operator == '+'){
-        result = num1+num2;
-    } 
-    else if (operator == '-'){
-        result = num1-num2;
-    } 
-    else if (operator == '*'){
-        result = num1*num2;
-    } 
-    else if (operator == '/'){
-      result = num1/num2;
-    } else {
-      result = 'Unknown operation';
-    }
-  return result;
-    }
-    
-    console.log(Calc('/', 9, 3));
-
-
-
-
-    let result;
-    
-    const isNotValid = (typeof num1 !== 'number' || typeof num2 !== 'number')
-    if (isNotValid){
-      result = 'Error';
-    }
-
-    let operations = {
-
-        sum: 'num1 + num2',
-        sub: 'num1 - num2',
-        multi: 'num * num2',
-        div: 'num1 / num2',
-      
-      }
-
-      return 'Unknown operation'
-
-      console.log(Calc('sum', 9, 3));
-
+console.log(Calc('multi', 2, 5));
+console.log(Calc('', 2, 5));
+console.log(Calc('div', 2, 5));
+console.log(Calc('sum', 2, 5));
+console.log(Calc('sub', 2, 5));
+console.log(Calc('div', "hi", 1));
